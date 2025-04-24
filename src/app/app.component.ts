1c2f50755934fdcc7abadd6529d15e1451ec2b7c
samanae <samia.rachid03@gmail.com>
2025-04-24 16:33:59 +0200

    save current state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { P5AnimationComponent } from '../assets/p5-animation/p5-animation.component';
+import { HomepageComponent } from './sites/homepage/homepage.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, P5AnimationComponent, NavigationComponent, HomepageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './sites/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'homepage', component: HomepageComponent    },
+    /* {path: 'projects', component: ProjectsComponent},
+    {path: 'about', component: AboutComponent} */
+];
