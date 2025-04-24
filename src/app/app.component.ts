fed3a73244ad06b47c3ee0d26a47cec32f4e79ce
samanae <samia.rachid03@gmail.com>
2025-04-24 17:12:58 +0200

    fixed navigation and added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { P5AnimationComponent } from '../assets/p5-animation/p5-animation.component';
-import { HomepageComponent } from './sites/homepage/homepage.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
+import { NavigationComponent } from '../components/navigation/navigation.component';
+import { HomepageComponent } from '../sites/homepage/homepage.component';
+import { ProjectsComponent } from '../sites/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, P5AnimationComponent, NavigationComponent, HomepageComponent],
+  imports: [RouterOutlet, RouterLink, P5AnimationComponent, NavigationComponent, HomepageComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomepageComponent } from './sites/homepage/homepage.component';
+import { HomepageComponent } from '../sites/homepage/homepage.component';
+import { ProjectsComponent } from '../sites/projects/projects.component';
 
 export const routes: Routes = [
-    {path: 'homepage', component: HomepageComponent    },
-    /* {path: 'projects', component: ProjectsComponent},
-    {path: 'about', component: AboutComponent} */
+    {path: 'homepage', component: HomepageComponent },
+    {path: 'projects', component: ProjectsComponent},
+    /* {path: 'about', component: AboutComponent} */
 ];
