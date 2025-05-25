1b5320bd721633e009c723e4d0f36d9fb17e6cee
samanae <samia.rachid03@gmail.com>
2025-05-25 11:40:33 +0200

    Auto-generated commit

--- assets/data/projects.ts
+++ assets/data/projects.ts
@@ -44,7 +44,7 @@ export const PROJECTS = [
     date: '2024 – today',
     description: 'A casual mobile and web-friendly number puzzle game. Designed and developed for a university course. The production is maintained outside of the projects lifespan due to enjoyment of creating games with web-frameworks. Designed by Samia Rachid.',
     disciplines: ['Game Design', 'App Design', 'App Development', 'Angular'],
-    link: 'https://samanae.github.io/nuco-game/home',
+    link: 'https://github.com/samanae/nuco-game',
     imgUrl: 'assets/images/nuco.png',
     },
     {
