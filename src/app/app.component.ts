bcfb3fb824c35eb84421af184f5dbf4fdffb3f59
samanae <samia.rachid03@gmail.com>
2026-06-19 03:38:42 +0200

    updated imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { P5AnimationComponent } from '../assets/p5-animation/p5-animation.component';
 import { NavigationComponent } from '../components/navigation/navigation.component';
-import { HomepageComponent } from '../sites/homepage/homepage.component';
-import { ProjectsComponent } from '../sites/projects/projects.component';
+import { HomepageComponent } from '../../homepage/homepage.component';
+import { ProjectsComponent } from '../../projects/projects.component';
 import { CommonModule } from '@angular/common';
 import { ProjectCardComponent } from '../components/project-card/project-card.component';
-import { AboutComponent } from '../sites/about/about.component';
+import { AboutComponent } from '../../about/about.component';
 
 @Component({
   selector: 'app-root',
